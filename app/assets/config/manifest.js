719e9951686fff76afa67dc979f6b47b0209b451
DaseinUXD <markmatthewsphd@gmail.com>
2018-08-17 21:37:29 -0500

    added bootstrap

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,5 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link_directory ../../../node_modules/bootstrap/dist/css .css
+//= link_directory ../../../node_modules/bootstrap/dist/js .js
