8ca7307ecac60f98e6afc0a376b20376ecd7c870
DaseinUXD <markmatthewsphd@gmail.com>
2018-08-23 15:42:31 -0500

    successfully created session controller for Log In

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   # skip_before_action :verify_authenticity_token
   protect_from_forgery prepend: true, with: :exception
-  include SessionsHelper
+  # include SessionsHelper
 
   def current_user
     User.find(session[:user_id]) if session[:user_id]
-    puts "application_controller current_user says: current_user created"
+    puts "application_controller.rb METHOD: current_user says: 'current_user created'"
   end
   helper_method :current_user
 
   private
-  def require_login
-    redirect_to root_path unless current_user
-  end
+  # def require_login
+  #   redirect_to root_path unless current_user
+  # end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,16 +9,34 @@ class SessionsController < ApplicationController
   end
 
   def new
+    puts 'Loaded NEW SESSION VIEW'
+
   end
 
-  def create
-    email = params['session']['email']
-    puts email
-    user = User.find_by email: params['session']['email'].downcase
-    if user && user.authenticate(params['session']['password'])
-      log_in user
-      redirect to user
+  def create # CREATE NEW SESSION
+    puts 'Called session CREATE method'
+    email = params['session']['email'].downcase
+    password = params['session']['password']
+
+    user = User.find_by_email(email).try(:authenticate, password)
+    if user
+      puts 'found user and authenticated '
+      flash[:notice] = "Welcome back #{user.first_name}.  You are now logged in."
+      redirect_to user
+    else
+      puts 'not found user'
+      flash[:alert] = "Ooops! The email and password combination entered did not work."
+      redirect_to login_path
     end
+    # puts 'this is the user trying to find by email',
+    # email = params['session']['email']
+    # puts email
+    # user = User.find_by email: params['session']['email'].downcase
+    # puts user
+    # if user && user.authenticate(params['session']['password'])
+    #   log_in user
+    #   redirect to user
+    # end
   end
 
   def destroy
